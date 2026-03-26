b3da7b4d1d58a72f7ca0a91ae8b835466fc8b10d
alan sojan <alansojan46@gmail.com>
2026-03-27 00:36:32 +0530

    added notification in navbar

--- analytics/aiAnalytics.service.js
+++
@@ -1,4 +0,0 @@
-// Re-export the pure AI helpers from the isolated server module.
-// Keeping this file satisfies the /analytics module layout while keeping logic in one place.
-export { calculateWasteRisk, predictDonationVolume } from "../server/services/aiAnalytics.service.js";
-
